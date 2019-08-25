14415d62d2791a54a0870d89ef4d1df6c5b0f5c1
burugaria7 <burugaria@icloud.com>
2019-08-25 15:58:42 +0900

    OpenCVの動画読み込みの動作チェックおｋ

--- makeSMF/makeSMF.cpp
+++ makeSMF/makeSMF.cpp
@@ -1,10 +1,27 @@
 ﻿#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
-	Mat image = Mat::zeros(100, 100, CV_8UC3);
-	imshow("", image);
-	waitKey(0);
+	cv::namedWindow("movie", cv::WINDOW_AUTOSIZE);
+	cv::VideoCapture cap;
+	cap.open("C:\\Users\\Retasu\\Documents\\Github\\makeSMF\\videos\\cho720.mp4");
+	if (!cap.isOpened()) {
+		cout << "path error\n" << endl;
+		return -1;
+	}
+
+	double fps = cap.get(cv::CAP_PROP_FPS);
+
+	cv::Mat frame;
+	for (;;) {
+		cap >> frame;
+		if (frame.empty()) break;
+		cv::imshow("movie", frame);
+		if ((char)cv::waitKey((int)1000 / fps) >= 0) break;
+	}
 }
