b90dbe5dfbb5c5d27bbda29e36b030467fb1f7c0
burugaria7 <burugaria@icloud.com>
2020-02-02 02:20:10 +0900

    もろもろの変更

--- makeSMF/analysis.cpp
+++ makeSMF/analysis.cpp
@@ -22,6 +22,8 @@ Analysis::Analysis()
 void Analysis::Set_Coodinates()
 {
 
+	#define YY 10;
+
 	cout << "Set_Coodinates() 座標セット関数" << endl;
 
 	//720p 88key
@@ -31,7 +33,7 @@ void Analysis::Set_Coodinates()
 
 		//white
 
-		this->key_white_y = 665;
+		this->key_white_y = 665-YY;
 
 		for (int i = 0; i < 52; i++) {
 			this->key_white_x[i] = (24.5 / 2.0) + i * 24.6;
@@ -39,7 +41,7 @@ void Analysis::Set_Coodinates()
 
 		//black
 
-		this->key_black_y = 620;
+		this->key_black_y = 620-YY;
 
 		this->key_black_x[0] = 28.5;
 
@@ -147,8 +149,8 @@ void Analysis::Analyze()
 		this->active_key_sum = 0;
 		cv::imshow("movie", frame);
 
-
-		if ((char)cv::waitKey((int)1000 / fps) >= 0) break;
+		//ここのコメントアウトはずすと動画速度になる
+		//if ((char)cv::waitKey((int)1000 / fps) >= 0) break;
 	}
 
 	//最後にファイルに書き出し
